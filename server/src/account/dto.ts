c7c95eed2a314cbc03f1644b6760d11d3ab05bdc
tntyyy <tnty.working@gmail.com>
2023-11-19 19:22:40 +0300

    feat: block-list module

--- server/src/account/dto.ts
+++ server/src/account/dto.ts
@@ -14,7 +14,9 @@ export class AccountDto {
 }
 
 export class PatchAccountDto {
-  @ApiProperty()
+  @ApiProperty({
+    required: false,
+  })
   @IsBoolean()
   @IsOptional()
   isBlockingEnabled?: boolean;
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { DbService } from "../db/db.service";
 import { AccountService } from "../account/account.service";
+import { BlockListService } from "../block-list/block-list.service";
 
 @Injectable()
 export class UsersService {
   constructor(
     private db: DbService,
     private accountService: AccountService,
+    private blockListService: BlockListService,
   ) {}
 
   findByEmail(email: string) {
@@ -17,6 +19,7 @@ export class UsersService {
     const user = await this.db.user.create({ data: { email, hash, salt } });
 
     await this.accountService.createAccount(user.id);
+    await this.blockListService.create(user.id);
 
     return user;
   }
