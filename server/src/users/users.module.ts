a692b798c26da086fc754f72c63fb5fbcb31adf3
tntyyy <tnty.working@gmail.com>
2023-11-19 18:40:06 +0300

    feat: account module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { DbModule } from "../db/db.module";
+import { AccountModule } from "../account/account.module";
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AccountModule],
   providers: [UsersService],
   exports: [UsersService],
 })
